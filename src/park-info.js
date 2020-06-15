117bc7a5212d00435cb46f5c5cbba2655c68bc0b
Joel <jchackosaji@gmail.com>
2020-06-15 16:01:34 -0400

    Added wiki and google result

--- src/park-info.js
+++ src/park-info.js
@@ -8,9 +8,22 @@ export default class ParkInfo extends PureComponent {
     return (
       <div>
         <div>
+          <br/>
+          Google:
+          <br/>
           <a
             target="_new"
             href={`https://www.google.com/search?q=${displayName}`}
+          >
+            {displayName}
+          </a>
+          <br/>
+          <br/>
+          Find more info on WIKI:
+          <br/>
+          <a
+            target="_new"
+            href={`https://en.wikipedia.org/wiki/${displayName}`}
           >
             {displayName}
           </a>
