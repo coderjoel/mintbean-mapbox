ea903620946d528df5b327fbbb0b968038437c27
Joel <jchackosaji@gmail.com>
2020-06-15 13:35:39 -0400

    Json Data from Parks Canada added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,204 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global document */
+import * as React from 'react';
+import {useState} from 'react';
+import MapGL, { Source, Layer} from 'react-map-gl';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-function App() {
+const geojson = require('./geodata/Canadian_National_Parks.geojson');
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiamNoYWNrb3NhamkiLCJhIjoiY2tiZ21pMThkMTZ4cTJ3bXJsNHdraGJmMSJ9.r-7cvl27blbieNlinE6S0g'; // Set your mapbox token here
+const MAPBOX_STYLE = 'mapbox://styles/jchackosaji/ckbgp6kta4x8x1jlztdr4b13e';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  toolbarStyle: {
+    backgroundColor: '#182F25'
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+}));
+
+export default function App() {
+  
+  const [state, setState] = useState({
+    viewport: {
+      latitude: 37.8,
+      longitude: -122.4,
+      zoom: 14,
+      bearing: 0,
+      pitch: 0
+    },
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const classes = useStyles();
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, ['left']: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer('left', false)}
+      onKeyDown={toggleDrawer('left', false)}
+    >
+      <List>
+        {['Canada', 'United States'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['About'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar
+        className={classes.toolbarStyle}>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer('left', true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Canadian National Parks
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      {
+        <React.Fragment key={'left'}>
+          <Drawer anchor={'left'} open={state['left']} onClose={toggleDrawer('left', false)}>
+            {list('left')}
+          </Drawer>
+        </React.Fragment>
+      }
+      <MapGL
+      {...state.viewport}
+      {...state.settings}
+      width="100vw"
+      height="100vh"
+      onViewportChange={viewport => setState({viewport})}
+      mapboxApiAccessToken={MAPBOX_TOKEN}
+      mapStyle={MAPBOX_STYLE}
+    >
+      <Source
+      id="geojson"
+      type="geojson"
+      data={geojson}
+      />
+      <Layer
+      id="anything"
+      type="fill"
+      source="geojson"
+      paint={{
+        "fill-color": 'red',
+        "fill-opacity": 0.4
+      }}
+      />
+      </MapGL>
     </div>
   );
 }
-
-export default App;
